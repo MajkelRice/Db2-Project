1ac4e2e202379b69efa686d9f20b6e33937a8c6d
Michal Ryz <michalryz2003@gmail.com>
2025-04-17 15:50:52 +0200

    init commit

---
+++ src/main/java/agh/ics/db2project/Db2ProjectApplication.java
@@ -0,0 +1,13 @@
+package agh.ics.db2project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Db2ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Db2ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/agh/ics/db2project/Db2ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package agh.ics.db2project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Db2ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
